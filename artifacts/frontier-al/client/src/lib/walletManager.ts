f3faf2a36b80c3e3e716719cbeb7bfa6240f88c0
Kudbee <dominick.ziola@gmail.com>
2026-05-31 19:56:33 -0500

    Refactor default network assignment in walletManager

--- artifacts/frontier-al/client/src/lib/walletManager.ts
+++ artifacts/frontier-al/client/src/lib/walletManager.ts
@@ -1,11 +1,10 @@
 import { WalletManager, WalletId, NetworkId } from "@txnlab/use-wallet";
-
+ 
+const network = import.meta.env.VITE_ALGORAND_NETWORK === "mainnet"
+ ? NetworkId.MAINNET
+ : NetworkId.TESTNET;
+ 
 export const walletManager = new WalletManager({
-  wallets: [
-    WalletId.PERA,
-    WalletId.DEFLY,
-    WalletId.KIBISIS,
-    WalletId.LUTE,
-  ],
-  defaultNetwork: NetworkId.TESTNET,
+ wallets: [WalletId.PERA, WalletId.DEFLY, WalletId.KIBISIS, WalletId.LUTE],
+ defaultNetwork: network,
 });
